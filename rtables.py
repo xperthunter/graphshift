93c1068faa67d3f0e2e43d407656047faa9703f1
xperthunter <kjfraga@ucdavis.edu>
2020-11-12 15:33:02 -0800

    tables script

---
+++ rtables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import csv
+
+print('aa,atom_type,functional_group,min,max')
+with open(sys.argv[1], mode='r') as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+	
+	for row in csv_reader:
+#		print(row)
+#		sys.exit()
+		aa  = row['Amino acid']
+		at  = row['atom type']
+		fg  = row['chemical group']
+		min = row['min']
+		max = row['max']
+		print(f'{aa},{at},{fg},{min},{max}')
+#		sys.exit()
